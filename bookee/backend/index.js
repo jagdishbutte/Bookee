4dfdffd6fbf6bb3ec3ebd1e62c4ddfb68e170750
Ashish kumar <ashishgupta020202@gmail.com>
2024-06-06 22:32:38 +0530

    starter file

---
+++ bookee/backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening at ${PORT}`);
+})
