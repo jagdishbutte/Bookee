39834c5ce0f2ce14d52ee3f69280e8c190bddedc
Ashish kumar <ashishgupta020202@gmail.com>
2024-08-11 12:04:36 +0530

    frontend-nav

---
+++ bookee/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function app(){
+  return (<>
+    {/* <div>hello</div> */}
+    <Navbar/>  
+  </>)
+}
+
+export default app;
